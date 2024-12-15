808f0ee9176dcb6e93bc80ed17c77ef1debd1b42
setsuna-0201 <setsuna.kunugiza@gmail.com>
2024-12-15 18:18:46 +0900

    Add files via upload

---
+++ dark-mord/js/script.js
@@ -0,0 +1,26 @@
+//btnを取得
+const btn=document.querySelector('#btn');
+//btnがクリックされる
+btn.addEventListener('click',()=>
+{
+    //クリックされた時の処理
+    document.body.classList.toggle('dark-theme');
+    if(btn.textContent==='ダークモードにする')
+    {
+        btn.textContent==='ライトモードにする';
+    }
+    else(btn.textContent==='ライトモードにする')
+    {
+        btn.textContent==='ダークモードにする';
+    }
+
+}
+)
+let ipt=document.querySelector('input');
+let btn2=document.querySelector('#btn2');
+//elemはp1の内容を表示する変数とする
+let elem=document.querySelector('p1');
+btn2.addEventListener('click',()=>
+{
+    elem.innerText=ipt.value;
+});
